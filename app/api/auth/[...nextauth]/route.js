0dd3189a6c79a0efb5cbaa38cce4d226036c0430
Pratik Jha <pratik04jha@gmail.com>
2025-04-13 21:04:31 +0530

    authorization error fix

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -6,8 +6,12 @@ const handler = NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      // Add the redirectUri here
+      authorizationUrl: 'https://github.com/login/oauth/authorize?scope=read:user',
+      redirectUri: `${process.env.NEXTAUTH_URL}/api/auth/callback/github`,  // Make sure this matches your registered GitHub redirect URI
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
